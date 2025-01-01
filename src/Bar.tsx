66351ed56a298c18404a337ad1cc79625d812152
ben <benvansleen@gmail.com>
2025-01-01 15:54:39 -0500

    Filter displayed workspaces by monitor

--- src/Bar.tsx
+++ src/Bar.tsx
@@ -2,10 +2,43 @@ import { App, Astal, Gtk, Gdk } from "astal/gtk3";
 import { Variable } from "astal";
 import Workspaces from "./Workspaces";
 import CpuHistogram from "./CpuHistogram";
+import { Index } from "./types";
 
-const time = Variable("").poll(1000, "date");
+const time = Variable("").poll(1000, "date '+%-I:%M %p'");
 
-export default function Bar(gdkmonitor: Gdk.Monitor) {
+type BarProps = {
+  gdkmonitor: Gdk.Monitor;
+  monitor_idx: Index;
+};
+
+function Left({ monitor_idx }: BarProps) {
+  return (
+    <box halign={Gtk.Align.START}>
+      <Workspaces monitor_idx={monitor_idx} />
+    </box>
+  );
+}
+
+function Center(_props: BarProps) {
+  return (
+    <box halign={Gtk.Align.CENTER}>
+      <CpuHistogram />
+    </box>
+  );
+}
+
+function Right(_props: BarProps) {
+  return (
+    <box halign={Gtk.Align.END}>
+      <button onClick={() => print("hello")} halign={Gtk.Align.END}>
+        <label label={time()} />
+      </button>
+    </box>
+  );
+}
+
+export default function Bar(gdkmonitor: Gdk.Monitor, monitor_idx: Index) {
+  const props = { gdkmonitor, monitor_idx };
   return (
     <window
       className="Bar"
@@ -19,13 +52,9 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
       application={App}
     >
       <centerbox>
-        <Workspaces />
-        <button onClicked="echo hello" halign={Gtk.Align.CENTER}>
-          <CpuHistogram />
-        </button>
-        <button onClick={() => print("hello")} halign={Gtk.Align.CENTER}>
-          <label label={time()} />
-        </button>
+        <Left {...props} />
+        <Center {...props} />
+        <Right {...props} />
       </centerbox>
     </window>
   );
--- src/Workspaces.tsx
+++ src/Workspaces.tsx
@@ -1,26 +1,38 @@
 import { bind, Variable } from "astal";
 import Hyprland from "gi://AstalHyprland";
+import { Index } from "./types";
 
 const hyprland = Hyprland.get_default();
 
-export default function Workspaces(): JSX.Element {
+type WorkspacesProps = {
+  monitor_idx: Index;
+};
+
+export default function Workspaces({
+  monitor_idx,
+}: WorkspacesProps): JSX.Element {
   const workspaces = Variable.derive(
     [
       bind(hyprland, "workspaces"),
       bind(hyprland, "focusedWorkspace"),
       bind(hyprland, "focusedClient"),
+      bind(hyprland, "monitors"),
     ],
-    (workspaces, focusedWorkspace, focusedClient) => {
-      print(workspaces.map((ws) => ws.get_name()).join(", "));
-
+    (workspaces, _focusedWorkspace, focusedClient, monitors) => {
+      const monitor = monitors.reverse()[monitor_idx];
+      const monitorActiveWorkspace = monitor.get_active_workspace();
       const workspace_buttons = workspaces
         .sort((a, b) => a.id - b.id)
-        .filter((ws) => !ws.get_name()?.startsWith("special"))
+        .filter(
+          (ws) =>
+            !ws.get_name()?.startsWith("special") &&
+            ws.get_monitor().get_name() == monitor.get_name(),
+        )
         .map((ws) => {
           return (
             <button
               onClick={() => ws.focus()}
-              className={ws.id === focusedWorkspace.id ? "focused" : ""}
+              className={ws.id === monitorActiveWorkspace.id ? "focused" : ""}
             >
               <label label={ws.get_name() ?? ""} />
             </button>
@@ -33,7 +45,11 @@ export default function Workspaces(): JSX.Element {
 
           {focusedClient ? (
             <button>
-              <label label={focusedClient.get_initial_class()} />
+              <label
+                label={monitorActiveWorkspace
+                  .get_last_client()
+                  .get_initial_class()}
+              />
             </button>
           ) : (
             <box />
@@ -43,5 +59,5 @@ export default function Workspaces(): JSX.Element {
     },
   );
 
-  return <box>{workspaces()}</box>;
+  return <>{workspaces()}</>;
 }
