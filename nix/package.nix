80e1cea8d90e53b2302fac466e57cf730c65bda7
ben <benvansleen@gmail.com>
2025-01-01 15:52:26 -0500

    Introduce cli utility to report ram usage

--- nix/package.nix
+++ nix/package.nix
@@ -8,11 +8,16 @@ ags.lib.bundle {
   inherit pkgs;
   src = ../.;
   name = "hyprbar";
-  entry = "app.ts";
+  entry = "./src";
   extraPackages =
     with pkgs;
     [
       sysstat
+      (writeShellScriptBin "ram-usage" ''
+        ${procps}/bin/free -m \
+          | ${gnugrep}/bin/grep 'Mem' \
+          | ${gawk}/bin/awk '{print $2, $3}'
+      '')
     ]
     ++ (with ags.packages.${system}; [
       hyprland
