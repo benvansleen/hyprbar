820802aab60ed2943ae2cf7e6c6370288291c312
ben <benvansleen@gmail.com>
2025-01-05 16:19:10 -0500

    Allow users to disable systemd service definition
    Defining systemd service requires building the package, which is not
    yet supported on aarch64 systems

--- nix/home-manager.nix
+++ nix/home-manager.nix
@@ -1,8 +1,21 @@
 { self, ... }:
-{ pkgs, lib, ... }:
-
 {
-  config = {
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  inherit (lib) mkIf mkEnableOption;
+  cfg = config.hyprbar;
+in
+{
+  options.hyprbar = {
+    enable = mkEnableOption "hyprbar";
+  };
+
+  config = mkIf cfg.enable {
     systemd.user.services.hyprbar = {
       Unit = {
         Description = "Run hyprbar";
