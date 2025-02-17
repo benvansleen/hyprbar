9d61e836686153af7817dc5a8966d269501a2343
ben <benvansleen@gmail.com>
2025-02-17 15:40:21 -0500

    Introduce `CpuTemperature` widget to poll real-time CPU temperature

---
+++ src/CpuTemperature.tsx
@@ -0,0 +1,21 @@
+import { Variable } from "astal";
+
+const cpuTemp: Variable<number> = Variable(0).poll(
+  1000,
+  ["cat", "/sys/class/hwmon/hwmon3/temp1_input"],
+  (temp: string, _prev: number) => {
+    return Number(temp) / 1000;
+  },
+);
+
+export default function CpuTemperature(): JSX.Element {
+  const widget = Variable.derive([cpuTemp], (cpuTemp) => {
+    return (
+      <button className="dial">
+        <label label={` ${cpuTemp.toFixed(0)}°C`} className="dial-label" />
+      </button>
+    );
+  });
+
+  return <>{widget()}</>;
+}
